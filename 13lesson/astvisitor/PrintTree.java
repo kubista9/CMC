da3df84db029493b35aa461e8b0746e0c2b4d716
Jakub Kuka <jakubkuka07@gmail.com>
2025-06-05 23:01:52 +0900

    Main method for printTree added

--- 13lesson/astvisitor/PrintTree.java
+++ 13lesson/astvisitor/PrintTree.java
@@ -4,21 +4,23 @@ public class PrintTree implements ASTVisitor {
 
 	@Override
 	public Object visitDoubleLiteral(ASTDoubleLiteral literal) {
-		// implement this part
-		//
-		// get the "void print(int indent)" from ASTDoubleLiteral class
-		// to implement
-		
+		literal.print(0);
 		return null;
 	}
 
 	@Override
 	public Object visitOperatorExpression(ASTOperatorExpression opexp) {
-		// implement this part
-		//
-		// get the "void print(int indent)" from ASTOperatorExpression class
-		// to implement
-		
+		opexp.print(0);
 		return null;
+	}
+
+	public static void main(String[] args) {
+		ASTDoubleLiteral num1 = new ASTDoubleLiteral(5.0);
+		ASTDoubleLiteral num2 = new ASTDoubleLiteral(3.0);
+		ASTOperatorExpression expr = new ASTOperatorExpression(num1, num2, "+");
+		
+		PrintTree printer = new PrintTree();
+		System.out.println("Using PrintTree visitor:");
+		expr.accept(printer);
 	}
 }
